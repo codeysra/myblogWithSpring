8ae9401c40d411cf180e43b7cc54563c9cacb10c
Yousra Saidani <yousaidani@gmail.com>
2018-06-28 16:27:20 -0400

    Successfully switched to PostgreSQL

--- src/main/java/com/ysrsdn/myblog/db/TestDBConnectionPostgres.java
+++ src/main/java/com/ysrsdn/myblog/db/TestDBConnectionPostgres.java
@@ -50,6 +50,13 @@ public class TestDBConnectionPostgres extends HttpServlet {
 			 while(rs.next()) {
 				 System.out.println(rs.getString(1) + " " + rs.getString(2) );
 			 }
+			 
+			 System.out.println();
+			  rs = myConn.prepareStatement("SELECT username, password, enabled FROM users ").executeQuery();
+			 
+			 while(rs.next()) {
+				 System.out.println(rs.getString(1) + " " + rs.getString(2) + " " +  rs.getString(3));
+			 }
  		} catch (Exception e) {
 			e.printStackTrace();
 		}finally {
